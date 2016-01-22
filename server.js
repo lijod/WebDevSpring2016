1aed4eb83d24373818f1346ab62e4dba32f72346
Lijo Daniel <lijodaniel@Lijos-MacBook-Pro.local>
2016-01-21 19:16:26 -0500

    Added public to context path

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+/**
+ *
+ */
+
 console.log("Starting server...");
 console.time("Node startup time");
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
@@ -13,5 +18,9 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 app.listen(port, ipaddress);
+
+
+
+
 console.log("Server started.");
 console.timeEnd("Node startup time"); 
