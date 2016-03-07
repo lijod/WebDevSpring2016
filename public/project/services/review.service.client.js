6f070c7f150061db9351be4d662ad38d745fd7a8
Lijo Daniel <ldaniel@healthedge.com>
2016-03-07 17:12:09 -0500

    list review

--- public/project/services/review.service.client.js
+++ public/project/services/review.service.client.js
@@ -9,21 +9,33 @@
 
         var reviews = [
             {"userId" : 234,
+                "title": "This is a test title 1",
                 "review" : "This is a test review for macbook pro. Product ID: 1219696697139",
-                rating: 4,
-                "gadgetId" : 1219696697139},
+                "rating": 4,
+                "gadgetId" : "1219696697139"},
 
             {"userId" : 345,
+                "title": "This is a test title 2",
                 "review" : "This is a test review for macbook pro. Product ID: 1219696697139",
-                rating: 4,
-                "gadgetId" : 1219696697139}
+                "rating": 4,
+                "gadgetId" : "1219696697139"}
         ];
 
         var api = {
-
+            findAllReviewsForGadget: findAllReviewsForGadget
         };
 
         return api;
+
+
+        function findAllReviewsForGadget(gadgetId){
+            var toReturn = reviews.filter(function(review, index, arr){
+                return (review.gadgetId === gadgetId);
+            });
+            return toReturn;
+        }
+
+
     }
 
 })();
--- public/project/views/search/review.controller.js
+++ public/project/views/search/review.controller.js
@@ -4,8 +4,12 @@
         .module("GadgetGuruApp")
         .controller("ReviewController", ReviewController);
 
-    function ReviewController($scope, $rootScope) {
+    function ReviewController($scope, $stateParams, $rootScope, ReviewService) {
         console.log($rootScope.user);
+        var productId = $stateParams.productId;
+        console.log(productId);
+        var reviews = ReviewService.findAllReviewsForGadget(productId);
+        $scope.reviews = reviews;
     }
 
 })();
