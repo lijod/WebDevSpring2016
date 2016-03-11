6e4185e85b230ae37685b976ffb65af976157330
Lijo Daniel <lijodaniel@Lijos-MacBook-Pro.local>
2016-03-10 21:39:07 -0500

    ui changes

--- public/project/services/gadget.service.client.js
+++ public/project/services/gadget.service.client.js
@@ -15,7 +15,9 @@
         return api;
 
         function getGadgetsByKeyword(keyword){
-            var url = "http://api.bestbuy.com/v1/products(name=(KEYWORD)*&type!=Music&type!=Movie&type!=BlackTie&type!=Software)?format=json&apiKey=762v7pj4r2xqka9nnhetu5gn"
+            keyword = encodeURIComponent(keyword);
+            var url = "http://api.bestbuy.com/v1/products(name=(KEYWORD)*&type!=Music" +
+                "&type!=Movie&type!=BlackTie&type!=Software)?format=json&apiKey=762v7pj4r2xqka9nnhetu5gn";
             var searchURL = url.replace("(KEYWORD)", keyword);
             return $http.get(searchURL);
         }
