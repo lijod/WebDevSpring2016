ef5f8dc7ddc9113784b8d727c5552d3ce0279b7b
Lijo Daniel <lijodaniel@Lijos-MacBook-Pro.local>
2016-03-31 00:13:55 -0400

    server.js updated for openshift mongo

--- server.js
+++ server.js
@@ -13,7 +13,20 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 var mongoose = require("mongoose");
-var db = mongoose.connect('mongodb://localhost/webdev2016-assignment');
+
+var connectionString = 'mongodb://localhost/webdev2016-assignment';
+
+// use remote connection string
+// if running in remote server
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+  connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+      process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+      process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+      process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+      process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
