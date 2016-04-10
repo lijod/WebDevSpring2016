6cd01fe7dc8a29013d48df14f9e1e9d87def1048
Lijo Daniel <lijodaniel@Lijos-MacBook-Pro.local>
2016-04-10 17:18:34 -0400

    like/unlike in profile

--- public/project/client/views/user/profile/like/like.controller.js
+++ public/project/client/views/user/profile/like/like.controller.js
@@ -10,16 +10,65 @@
         function init() {
             var userId = $stateParams.userId;
 
-            UserService.findLikedGadgets(userId)
+            UserService.getCurrentUser()
+                .then(function (response) {
+                        vm.currUser = response.data;
+                        return UserService.findUserByUserId(vm.currUser._id);
+                    },
+                    function () {
+                        console.log("FollowerController->getCurrentUser");
+                    })
+                .then(function (response) {
+                        vm.currUser = response.data;
+                        return UserService.findLikedGadgets(userId);
+                    },
+                    function () {
+                        console.log("FollowerController->findUserByUserId");
+                    })
                 .then(function(response) {
-                        vm.likedGadgets = response.data;
+                        var likedGadgets = response.data;
+                        likedGadgets.forEach(function(element, index) {
+                           element.isLiked = vm.currUser.likedGadget.indexOf(element._id) > -1;
+                        });
+                    console.log(likedGadgets);
+                        vm.likedGadgets = likedGadgets;
                     },
                     function(err) {
                         console.log("ProfileLikeController->init->findLikedGadgets");
                     });
+
+            vm.likeGadget = likeGadget;
+            vm.undoLikeGadget = undoLikeGadget;
         }
 
         init();
+
+        function likeGadget(gadget) {
+            UserService.addLikedGadget(vm.currUser._id, gadget._id)
+                .then(function (response) {
+                        response = response.data;
+                        if (response.ok && response.ok === 1 && response.nModified && response.nModified === 1) {
+                            gadget.isLiked = true;
+                        }
+                    },
+                    function (err) {
+                        console.log(err);
+                    });
+        }
+
+        function undoLikeGadget(gadget) {
+            UserService.undoLikedGadget(vm.currUser._id, gadget._id)
+                .then(function (response) {
+                        response = response.data;
+                        console.log(response);
+                        if (response.ok && response.ok === 1 && response.nModified && response.nModified === 1) {
+                            gadget.isLiked = false;
+                        }
+                    },
+                    function (err) {
+                        console.log(err);
+                    });
+        }
     }
 
 })();
