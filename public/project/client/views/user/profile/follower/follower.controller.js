3d2819a7b69743420dcfeeb4d3b6b3f4d8129df1
Lijo Daniel <lijodaniel@Lijos-MacBook-Pro.local>
2016-04-10 16:14:25 -0400

    follow/following from profile

--- public/project/client/views/user/profile/follower/follower.controller.js
+++ public/project/client/views/user/profile/follower/follower.controller.js
@@ -8,18 +8,60 @@
         var vm = this;
 
         function init() {
-            var userId = $stateParams.userId;
+            vm.paramUserId = $stateParams.userId;
 
-            UserService.findFollowerUsers(userId)
+            UserService.getCurrentUser()
+                .then(function (response) {
+                        vm.currUser = response.data;
+                        return UserService.findFollowerUsers(vm.paramUserId);
+                    },
+                    function () {
+                        console.log("FollowerController->getCurrentUser");
+                    })
                 .then(function(response) {
-                        vm.following = response.data;
+                        var followers = response.data;
+                        followers.forEach(function(element, index) {
+                            element.isFollowing = element.follower.indexOf(vm.currUser._id) > -1;
+                        });
+                        vm.follower = followers;
+                        console.log(vm.follower);
                     },
                     function(err) {
                         console.log("FollowerController->init->findFollowingUsers")
                     });
+
+            vm.follow = follow;
+            vm.unfollow = unfollow;
         }
 
         init();
+
+
+        function follow(toFollowUser) {
+            UserService.follow(vm.currUser._id, toFollowUser._id)
+                .then(function (response) {
+                        response = response.data;
+                        if (response && response.ok && response.ok === 1 && response.nModified && response.nModified === 1) {
+                            toFollowUser.isFollowing = true;
+                        }
+                    },
+                    function (err) {
+                        console.log(err);
+                    });
+        }
+
+        function unfollow(toUnfollowUser) {
+            UserService.unfollow(vm.currUser._id, toUnfollowUser._id)
+                .then(function (response) {
+                        response = response.data;
+                        if (response && response.ok && response.ok === 1 && response.nModified && response.nModified === 1) {
+                            toUnfollowUser.isFollowing = false;
+                        }
+                    },
+                    function (err) {
+                        console.log(err);
+                    });
+        }
     }
 
 })();
--- public/project/client/views/user/profile/follower/following.controller.js
+++ public/project/client/views/user/profile/follower/following.controller.js
@@ -8,18 +8,60 @@
         var vm = this;
 
         function init() {
-            var userId = $stateParams.userId;
+            vm.paramUserId = $stateParams.userId;
 
-            UserService.findFollowingUsers(userId)
+            UserService.getCurrentUser()
+                .then(function (response) {
+                        vm.currUser = response.data;
+                        return UserService.findFollowingUsers(vm.paramUserId);
+                    },
+                    function () {
+                        console.log("FollowingController->getCurrentUser");
+                    })
                 .then(function(response) {
-                    vm.following = response.data;
-                },
-                function(err) {
-                    console.log("FollowingController->init->findFollowingUsers")
-                });
+                        var following = response.data;
+                        following.forEach(function(element, index) {
+                            element.isFollowing = element.follower.indexOf(vm.currUser._id) > -1;
+                        });
+                        vm.following = following;
+                        console.log(vm.following);
+                    },
+                    function(err) {
+                        console.log("FollowerController->init->findFollowingUsers")
+                    });
+
+            vm.follow = follow;
+            vm.unfollow = unfollow;
+
         }
 
         init();
+
+        function follow(toFollowUser) {
+            UserService.follow(vm.currUser._id, toFollowUser._id)
+                .then(function (response) {
+                        response = response.data;
+                        if (response && response.ok && response.ok === 1 && response.nModified && response.nModified === 1) {
+                            toFollowUser.isFollowing = true;
+                        }
+                    },
+                    function (err) {
+                        console.log(err);
+                    });
+        }
+
+        function unfollow(toUnfollowUser) {
+            UserService.unfollow(vm.currUser._id, toUnfollowUser._id)
+                .then(function (response) {
+                        response = response.data;
+                        if (response && response.ok && response.ok === 1 && response.nModified && response.nModified === 1) {
+                            toUnfollowUser.isFollowing = false;
+                        }
+                    },
+                    function (err) {
+                        console.log(err);
+                    });
+        }
     }
 
 })();
