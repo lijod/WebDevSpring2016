a547f5d808d5921658ce7425babbac981c36d7e5
Lijo Daniel <ldaniel@healthedge.com>
2016-04-18 09:40:38 -0400

    search on keppress enter

--- public/project/client/views/search/search.controller.js
+++ public/project/client/views/search/search.controller.js
@@ -4,13 +4,20 @@
         .module("GadgetGuruApp")
         .controller("SearchController", SearchController);
 
-    function SearchController($http, $scope, GadgetService){
+    function SearchController($state){
         console.log("SearchController");
         var vm = this;
         vm.updateGadgetName = updateGadgetName;
+        vm.search = search;
 
         function updateGadgetName(gadgetName) {
             vm.gadgetName = gadgetName;
+        }
+
+        function search(event, keyword) {
+            if (event.keyCode === 13) {
+                $state.go("home.result", {keyword: keyword, isCategory: false});
+            }
         }
     }
 })();
